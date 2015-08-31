b042f91d2688285447a699757339878f66ec6bf4
ragarmakhis <ragarmakhis@gmail.com>
2015-08-31 16:46:27 +0300

    Initial Commit

---
+++ AstroCalc/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AstroCalc
+//
+//  Created by Maksim Roshtshin on 31.08.15.
+//  Copyright (c) 2015 ragarmakhis. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ AstroCalc/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  AstroCalc
+//
+//  Created by Maksim Roshtshin on 31.08.15.
+//  Copyright (c) 2015 ragarmakhis. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
